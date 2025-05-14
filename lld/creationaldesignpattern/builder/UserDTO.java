b5bb1e2d121ae005aed2cfcc3a3640ea9d21e486
= <vkrishna@securonix.com>
2025-05-14 18:04:58 +0530

    sec: Add builder creation pattern

---
+++ lld/creationaldesignpattern/builder/UserDTO.java
@@ -0,0 +1,11 @@
+package lld.creationaldesignpattern.builder;
+
+//Interface implemented by "products"
+public interface UserDTO {
+	
+	String getName();
+
+	String getAddress();
+
+	String getAge();
+}
